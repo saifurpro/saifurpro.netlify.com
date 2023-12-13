72fa0f7fce0fd9b580fcb6b3c8df626308cdf76d
Md. Saifur Rahaman <saifur703@gmail.com>
2023-12-14 00:11:29 +0600

    update-full-design

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,6 +1,9 @@
 !(function (e) {
   "use strict";
   jQuery(document).ready(function (e) {
+
+    $('#menu').slicknav();
+
     $(".latest-portfolio").owlCarousel({
       margin: 30,
       loop: true,
@@ -39,66 +42,10 @@
             items: 1,
           },
           768: {
-            items: 2,
+            items: 1,
           },
         },
       });
-
-    /* Load More */
-    let size_li = $(".portfolio .col-md-6").length;
-    let x = 6;
-    $(".portfolio .col-md-6:lt(" + x + ")").show();
-    $("#loadMore").click(function (e) {
-      e.preventDefault();
-      x = x + 6 <= size_li ? x + 6 : size_li;
-      $(".portfolio .col-md-6:lt(" + x + ")").show();
-      if (x == size_li) {
-        $("#loadMore").hide();
-      }
-      if (x > 6) {
-        $("#showLess").show();
-      }
-    });
-    $("#showLess").click(function (e) {
-      e.preventDefault();
-      x = x - 6 < 0 ? 6 : x - 6;
-      $(".portfolio .col-md-6")
-        .not(":lt(" + x + ")")
-        .hide();
-      if (x <= 6) {
-        $("#showLess").hide();
-      }
-      if (x < size_li) {
-        $("#loadMore").show();
-      }
-    });
-
-    /* Load More */
-    
-    /* 2nd Load More */
-    let reviews_li = $(".reviews .col-md-12").length;
-    let xx = 6;
-    $(".reviews .col-md-12:lt(" + xx + ")").show();
-    $("#loadMoreBtn").click(function (e) {
-      e.preventDefault();
-      xx = xx + 6 <= reviews_li ? xx + 6 : reviews_li;
-      $(".reviews .col-md-12:lt(" + xx + ")").show();
-      if (xx == reviews_li) {
-        $("#loadMoreBtn").hide();
-      }
-      if (xx != reviews_li) {
-        $(".load-more-wrap h5").text(
-          reviews_li - xx + " more testimonials left"
-        );
-      } else {
-        $(".load-more-wrap h5").text(
-          "No more testimonials available. Will update after completing any project"
-        );
-        $(".load-more-wrap h5").css({ color: "red" });
-      }
-      console.log(xx, reviews_li);
-    });
-    /* 2nd Load More */
     
   });
   
